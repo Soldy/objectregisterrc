30628c9138208d82e391c722bc370dbdfdd825ee
Soldy <4786022+Soldy@users.noreply.github.com>
2020-10-08 20:50:17 +0100

    type hint
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,23 +1,38 @@
 
 const registerBase = function(){
+    /*
+    * @public
+    */
     this.live = function(){
         return true;
     }
+    /*
+    * @public
+    */
     this.version = function(name){
          if(typeof objectDb[name] === 'undefined')
              return false;
          return objectDb[name].version;
     }
+    /*
+    * @public
+    */
     this.get = function(name){
          if(typeof objectDb[name] === 'undefined')
              return false;
          return objectDb[name].object;
     }
+    /*
+    * @public
+    */
     this.check = function(name){
          if(typeof objectDb[name] !== 'undefined')
              return true;
          return false;
     }
+    /*
+    * @public
+    */
     this.add = function(name, version, object){
          if(typeof objectDb[name] !== 'undefined')
              return false;
@@ -27,6 +42,9 @@ const registerBase = function(){
          }
          return true;
     }
+    /*
+    * @public
+    */
     let objectDb = {};
 }
 
